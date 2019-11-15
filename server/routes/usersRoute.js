8610958b638ffc0d3020a8f0e61de87669ff0dfd
peln <pern9378@stud.kea.dk>
2019-11-15 12:42:32 +0100

    detached head

--- server/routes/usersRoute.js
+++ server/routes/usersRoute.js
@@ -5,12 +5,25 @@ const User = require('../models/User');
 router.get('/users', async (req, res) => {
     console.log('***** users route *******');
     // const users = await users.query().select();
-    const newUser = {
-        email: "test2@mail.com"
-    };
-    await User.query().insert(newUser);
-    const users = await User.query().column('email').select();
+    // const newUser = {
+    //     email: "test2@mail.com"
+    // };
+    // await User.query().insert(newUser);
+    // const users = await User.query().column('email').select();
+    const users = await User.query().select();
     res.json(users);
 });
+
+
+router.post('/users/signup', async (req, res) => {
+    console.log('***** signup *******');
+    if(req.body.username && req.body.password){
+        console.log(req.body);
+        
+    
+    }
+
+});
+
 
 module.exports = router;
