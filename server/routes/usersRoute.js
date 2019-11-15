2ddeeea1a981c90ed725f25061a3b6f15ddfc74b
peln <pern9378@stud.kea.dk>
2019-11-15 16:36:06 +0100

    signup and login works

--- server/routes/usersRoute.js
+++ server/routes/usersRoute.js
@@ -3,51 +3,37 @@ const router = express.Router();
 const User = require('../models/User');
 
 router.get('/users', async (req, res) => {
-    console.log('***** users route *******');
-    // const users = await users.query().select();
-    // const newUser = {
-    //     email: "test2@mail.com"
-    // };
-    // await User.query().insert(newUser);
-    // const users = await User.query().column('email').select();
+    console.log('***** get all users *******');   
     const users = await User.query().select();
     res.json(users);
 });
 
-// create a new user with post
-// router.post('/users/signup', async (req, res) => {
-//     console.log(req.body.email);
-//     // todo validate the input
-//     if(req.body.email && req.body.password) {
-        
-//         const newUser = {
-//             email: req.body.email,
-//             password: req.body.password
-//         }
-//         // insert user
-//         const user = await User.query().insert(newUser);
-//         res.status(200).json({message: user});
-//     } else {
-//         res.status(400).json({response: "Missing password"});
-//     }
-// });
-
-
-// Check if post is working
 router.post('/users/login', async (req, res) => {
-    console.log(req.body)
-    res.json({});
+    console.log('***** login *******');   
+    const validUser = await User.query().select().where({ email: req.body.email });
+    console.log(validUser);
+    if(validUser.length > 0){
+        res.status(200).json({message: "You have been logged in"});
+    } else {
+        res.status(400).json({message: "Something went wrong"});
+    }
 });
 
 router.post('/users/signup', async (req, res) => {
     console.log('***** signup *******');
-    if(req.body.username && req.body.password){
-        console.log(req.body);
-        
-    
+    // console.log(req.body);
+    if(req.body.email && req.body.password){        
+        const newUser = {
+            email: req.body.email,
+            password: req.body.email
+        }
+
+        const user = await User.query().insert(newUser);
+        res.status(200).json({ message: user });
+
+    } else {
+        res.status(400).json({ response: "missing password" });
     }
-
 });
-
 
 module.exports = router;
