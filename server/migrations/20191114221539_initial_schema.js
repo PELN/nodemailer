78b3605d0feff3d786a297342d045923bc36e63f
peln <pern9378@stud.kea.dk>
2019-11-15 10:25:11 +0100

    table emails created in db

--- server/migrations/20191114221539_initial_schema.js
+++ server/migrations/20191114221539_initial_schema.js
@@ -3,12 +3,20 @@ exports.up = function(knex) {
   return knex.schema
     .createTable('users', (table) => {
         table.increments('id');
-        table.string('email');
+        table.string('email').unique();
         table.string('password');
     })
+    .createTable('emails', (table) => {
+        table.increments('id');
+        table.integer('user_id').unsigned();
+        table.foreign('user_id').references('users.id');
+        table.string('subject');
+        table.string('message');
+    });
 };
 
 exports.down = function(knex) {
     return knex.schema
-        .dropTableIfExists('users');
+        .dropTableIfExists('users')
+        .dropTableIfExists('emails');
 };
