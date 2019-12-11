d68b8614310548751c40a1db7273a05cd069e8d4
PELN <31202787+PELN@users.noreply.github.com>
2019-12-11 15:02:11 +0100

    Delete mysql_credentials.js

--- server/mysql_config/mysql_credentials.js
+++
@@ -1,6 +0,0 @@
-const database = 'nodemailer';
-const user = 'root';
-const password = 'Pern2';
-
-// export key as database, with the variables as value
-module.exports = { database, user, password };
